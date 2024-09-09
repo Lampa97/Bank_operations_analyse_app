96c9c2e2bab6a523e605cd17ba2d4cf51cb4db0b
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-09-09 12:00:09 +0300

    added logger

--- src/reports.py
+++ src/reports.py
@@ -1,22 +1,36 @@
-from idlelib.pyparse import trans
-from typing import Optional
-import pandas as pd
-import logging
 import datetime
-import re
+from typing import Optional
 
-def spending_by_category(transactions: pd.DataFrame,
-                         category: str,
-                         date: Optional[str] = None) -> pd.DataFrame:
+import pandas as pd
+
+from src.logger import logger_setup
+
+logger = logger_setup()
+
+
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+    """Позволяет отфильтровать транзакции по описанию за последние 3 месяца с указанной даты"""
     if date:
         current_date = datetime.datetime.strptime(date, "%d.%m.%Y")
     else:
         current_date = datetime.datetime.today()
-    transactions['TimeStamp'] = pd.to_datetime(transactions['Дата платежа'], dayfirst=True)
-    transactions['Сегодня'] = current_date
-    transactions['Прошло дней'] = (transactions['Сегодня'] - transactions['TimeStamp']).dt.days
-    filtered_info = transactions.loc[(transactions['Категория'] == category) & (transactions['Прошло дней'] < 90) & (transactions['Прошло дней'] >= 0)]
-    del filtered_info['Сегодня']
-    del filtered_info['Прошло дней']
-    del filtered_info['TimeStamp']
+    logger.info(f"Получена дата: {current_date}")
+    transactions["TimeStamp"] = pd.to_datetime(transactions["Дата платежа"], dayfirst=True)
+    logger.info("Создали столбец 'TimeStamp'")
+    transactions["Сегодня"] = current_date
+    logger.info("Создали столбец 'Сегодня'")
+    transactions["Прошло дней"] = (transactions["Сегодня"] - transactions["TimeStamp"]).dt.days
+    logger.info("Создали столбец 'Прошло дней'")
+    filtered_info = transactions.loc[
+        (transactions["Категория"] == category)
+        & (transactions["Прошло дней"] < 90)
+        & (transactions["Прошло дней"] >= 0)
+    ]
+    logger.info(f"Отфильтровали транзакции по категории '{category}' за последние 3 месяца")
+    del filtered_info["Сегодня"]
+    logger.info("Удалили столбец 'Сегодня'")
+    del filtered_info["Прошло дней"]
+    logger.info("Удалили столбец 'Прошло дней'")
+    del filtered_info["TimeStamp"]
+    logger.info("Удалили столбец 'TimeStamp'")
     return filtered_info
